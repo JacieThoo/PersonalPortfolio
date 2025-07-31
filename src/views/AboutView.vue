4a6a93555c867929d358c6bf87bc5a3cab846916
JacieThoo <s10260236@connect.np.edu.sg>
2025-07-31 08:39:24 +0800

    Update Education

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -105,22 +105,52 @@ export default {
       <section
         class="text-sm md:text-lg text-justify flex flex-col gap-4 md:flex-row md:gap-8 md:justify-left md:items-center">
         <div class="flex justify-center">
-          <img class="w-9/12 rounded-full mb-3 fadein-up"
-            src="/img/avatar.png" alt="Foto">
+          <img class="w-9/12 rounded-full mb-3 fadein-up" src="/img/avatar.png" alt="Foto">
         </div>
         <div class="md:w-7/12">
           <p class="mb-3 md:mb-7 fadein-left fadeins-1">
-            Hello! I’m Jacie, a Year 3 student at Ngee Ann Polytechnic, pursuing a diploma in Immersive Media. 
+            Hello! I’m Jacie, a Year 3 student at Ngee Ann Polytechnic, pursuing a diploma in Immersive Media.
             I am passionate about interactive experiences and the ever-evolving world of immersive technology.
           </p>
           <p class="mb-3 fadein-left fadeins-2">
-            As I continue my journey, I’m eager to expand my skills and experiment with new tools. 
+            As I continue my journey, I’m eager to expand my skills and experiment with new tools.
             I’m always open to learning, collaborating, and connecting with others!
           </p>
         </div>
 
       </section>
 
+    </article>
+  </div>
+
+    <div class="px-5 py-5 md:px-12 md:py-10 text-left text-amber-50 mx-3">
+    <article data-page="education">
+      <header>
+        <div class="text-2xl font-bold text-white mb-5 fadein-bot title-section flex items-center">
+          <div class="h-[1px] w-10 bg-amber-200 md:w-20 aos-init aos-animate" data-aos="zoom-in-left"
+            data-aos-duration="600"></div>
+          &nbsp; Education
+        </div>
+      </header>
+
+      <section class="timeline space-y-6 pb-1">
+        <!-- Education Item 1 -->
+        <div class="border-l-2 border-amber-200 pl-5 relative">
+          <div class="absolute top-0 left-[-10px] w-4 h-4 bg-amber-200 rounded-full"></div>
+          <h3 class="text-xl font-regular text-white">Diploma in Immersive Media</h3>
+          <p class="text-sm text-amber-100 italic mb-1">Ngee Ann Polytechnic, Singapore</p>
+          <p class="text-sm text-gray-300">2023 - Present</p>
+          <p class="text-sm text-gray-400">Minor in Korean Language</p>
+        </div>
+
+        <!-- Education Item 2 -->
+        <div class="border-l-2 border-amber-200 pl-5 relative">
+          <div class="absolute top-0 left-[-10px] w-4 h-4 bg-amber-200 rounded-full"></div>
+          <h3 class="text-xl font-regular text-white">GCE O-Level</h3>
+          <p class="text-sm text-amber-100 italic mb-1">Chua Chu Kang Secondary School, Singapore</p>
+          <p class="text-sm text-gray-300">2019 – 2022</p>
+        </div>
+      </section>
     </article>
   </div>
 
@@ -138,11 +168,13 @@ export default {
           <ul class="flex flex-wrap text-sm font-medium text-center text-gray-500 dark:text-gray-400 mb-5">
             <li class="mr-2">
               <button class="inline-block px-4 py-3 rounded-lg hover:text-white"
-                :class="{ 'text-amber-200 bg-amber-200 bg-opacity-10': activeTab === 1 }" @click="activeTab = 1">Creative Tools</button>
+                :class="{ 'text-amber-200 bg-amber-200 bg-opacity-10': activeTab === 1 }"
+                @click="activeTab = 1">Creative Tools</button>
             </li>
             <li class="mr-2">
               <button class="inline-block px-4 py-3 rounded-lg hover:text-white"
-                :class="{ 'text-amber-200 bg-amber-200 bg-opacity-10': activeTab === 2 }" @click="activeTab = 2">Development Tools</button>
+                :class="{ 'text-amber-200 bg-amber-200 bg-opacity-10': activeTab === 2 }"
+                @click="activeTab = 2">Development Tools</button>
             </li>
           </ul>
         </div>
@@ -157,7 +189,8 @@ export default {
                     :src="item.imageUrl" style="color: transparent;">
                 </div>
                 <div class="flex items-center text-sm md:text-base lg:text-lg">
-                  <div class="tech font-medium text-secondary transition-all duration-300 translate-y-0 ">{{ item.name }}
+                  <div class="tech font-medium text-secondary transition-all duration-300 translate-y-0 ">{{ item.name
+                    }}
                   </div>
                   <div
                     class="status-tech opacity-0 absolute mt-5 text-[10px] text-amber-200 transition-all duration-300 md:text-xs lg:text-sm">
@@ -178,7 +211,8 @@ export default {
                     :src="item.imageUrl" style="color: transparent;">
                 </div>
                 <div class="flex items-center text-sm md:text-base lg:text-lg">
-                  <div class="tech font-medium text-secondary transition-all duration-300 translate-y-0 ">{{ item.name }}
+                  <div class="tech font-medium text-secondary transition-all duration-300 translate-y-0 ">{{ item.name
+                    }}
                   </div>
                   <div
                     class="status-tech opacity-0 absolute mt-5 text-[10px] text-amber-200 transition-all duration-300 md:text-xs lg:text-sm">
